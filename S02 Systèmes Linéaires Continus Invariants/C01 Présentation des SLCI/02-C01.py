e01563002328df83449b62e30bdd762cd6845d60
costadoat <costadoat@crans.org>
2022-10-14 17:25:00 +0200

    S02C01

--- S02 Systèmes Linéaires Continus Invariants/C01 Présentation des SLCI/02-C01.py
+++ S02 Systèmes Linéaires Continus Invariants/C01 Présentation des SLCI/02-C01.py
@@ -6,8 +6,9 @@ import numpy as np
 
 K=1
 tau=1
+E=1 # valeur de l'échelon de l'entrée
 
-H=lti([K],[tau,1])              #creation d'une instance de la classe
+H=lti([K*E],[tau,1])              #creation d'une instance de la classe
 t,y=H.step()                    #appel de la méthode step de la classe lti
 
 plt.plot(t,y)                       #affichage
