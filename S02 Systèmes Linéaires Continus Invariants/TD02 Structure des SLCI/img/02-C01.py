aac41ffd8193a6ada738628fdb8e0f1997a25540
costadoat <costadoat@crans.org>
2023-10-09 14:04:53 +0200

    TDs

--- S02 Systèmes Linéaires Continus Invariants/TD02 Structure des SLCI/img/02-C01.py
+++ S02 Systèmes Linéaires Continus Invariants/TD02 Structure des SLCI/img/02-C01.py
@@ -7,10 +7,10 @@ import numpy as np
 K=200
 Kv=0.01
 Ka=Kv/0.7
-K1=1.66
+K1=1.7
 tau1=30
 
-K2=K*Ka*K1/(1+K*K1*Kv)
+K2=1
 tau2=tau1/(1+K*K1*Kv)
 E=1 # valeur de l'échelon de l'entrée
 
@@ -18,5 +18,17 @@ H=lti([K2*E],[tau2,1])              #creation d'une instance de la classe
 t,y=H.step()                    #appel de la méthode step de la classe lti
 plt.plot(t,y)
 plt.grid('on')
-plt.show()
+
+plt.savefig('motoreduc_courbe.eps')
+
+H=lti([K2*E],[tau2,1])              #creation d'une instance de la classe
+t,y=H.step()                    #appel de la méthode step de la classe lti
+plt.plot([0,t[-1]],[1,1])
+plt.plot([0,tau2],[0,1])
+plt.plot([tau2,tau2],[0,1])
+plt.grid('on')
+
+
+plt.savefig('motoreduc_courbe_cor.eps')
+
 
