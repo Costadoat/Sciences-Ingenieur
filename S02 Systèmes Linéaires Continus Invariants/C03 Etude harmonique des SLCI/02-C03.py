384477d17744f3b43b12a4bbf1f12df683cdba26
costadoat <costadoat@crans.org>
2023-11-10 16:08:45 +0100

    pythons03

--- S02 Systèmes Linéaires Continus Invariants/C03 Etude harmonique des SLCI/02-C03.py
+++ S02 Systèmes Linéaires Continus Invariants/C03 Etude harmonique des SLCI/02-C03.py
@@ -1,63 +1,41 @@
 # On importe les bibliothèques
-from matplotlib.pyplot import *
-from numpy import *
-from pylab import *
+from matplotlib.pyplot import show, figure, subplot, gca
+from numpy import log10, arange, absolute
+from pylab import angle, semilogx
+from matplotlib.widgets import TextBox
 
-
-K=10
-w0=0.2
+# Paramétrage de la fonction de transfert
+K=1
+w0=100
 xi=1
 
-def H(w,w0,xi,k):
-    return K/(1j*w*(1+1j*w/w0)),w0
+def H(w,w0,xi,K):
+    return K/(1+2*xi*1j*w/w0-w**2/w0**2)
 
 fig=figure('Diagrammes Bode')
-puissance_w=arange(log10(w0)-3,log10(w0)+3,0.01)
-# Les pulsations w
-W=10**puissance_w
-# La phase en degré
-phase = angle(H(W,w0,xi,K)[0],'deg')
+# Calcul des valeurs de l'abscisse
+puissances_w=arange(log10(w0)-3,log10(w0)+3,0.01)
+W=10**puissances_w
 # Le module en dB
-module = 20*log10(absolute(H(W,w0,xi,K)[0]))
-#Tracer du diagramme de Bode
-subplot(211) # Perme    t d’afficher plusieurs graphes (nombre de graphe (2), colonne (1), ligne (1))
-diag_mod1,diag_mod2=semilogx(W,module,10,1) # Tracé en semilog du module
-axes = gca()
-axes.set_xlim(w0/1000,1000*w0)
-delta=(max(module)-min(module))/10
-axes.set_ylim(((min(module)-delta)//10)*10, ((max(module)+delta)//10+1)*10)
-axes.set_ylabel('Gain (dB)')
-axes.set_xscale('log')
-major_ticks_y = np.arange(((min(module)-delta)//10-1)*10, ((max(module)+delta)//10+1)*10+1, (((max(module)-min(module))/5)//5)*5)
-minor_ticks_y = np.arange(((min(module)-delta)//10-1)*10, ((max(module)+delta)//10+1)*10+1, (((max(module)-min(module))/20)//5)*5)
-axes.set_yticks(major_ticks_y)
-axes.set_yticks(minor_ticks_y, minor=True)
-axes.grid(True,which='minor', alpha=0.2)
-axes.grid(True,which='major', alpha=0.5)
+module = 20*log10(absolute(H(W,w0,xi,K)))
+# La phase en degré
+phase = angle(H(W,w0,xi,K),'deg')
+#Tracer des diagrammes de Bode
+subplot(211) # Permet d’afficher plusieurs graphes (nombre de graphe (2), colonne (1), ligne (1))
+diag_mod,val=semilogx(W,module,10,1) # Tracé en semilog du module
+# Paramétrage de la grille pour le module
+axes_mod = gca()
+axes_mod.grid(True,which='minor', alpha=0.2)
+axes_mod.grid(True,which='major', alpha=0.5)
+axes_mod.set_ylabel('Gain (dB)')
+axes_mod.set_xscale('log')
 subplot(212)
-diag_pha1,diag_pha2=semilogx(W,phase,10,1) #Tracé en semilog du module
-axes = gca()
-axes.set_xlim(w0/1000,1000*w0)
-axes.set_ylim((min(phase)//30)*30-30, (max(phase)//30*30)+30)
-axes.set_ylabel('Phase (deg)')
-axes.set_xlabel('Pulsation $(rad.s^{-1})$')
-major_ticks_y = np.arange((min(phase)//30*30)-30, ((max(phase)//30+1)*30)+30, 30)
-minor_ticks_y = np.arange((min(phase)//30*30)-30, ((max(phase)//30+1)*30)+30, 15)
-axes.set_yticks(major_ticks_y)
-axes.set_yticks(minor_ticks_y, minor=True)
-axes.grid(True,which='minor', alpha=0.2)
-axes.grid(True,which='major', alpha=0.5)
-
-barre_z = plt.axes([0.2, 0.1, 0.4, 0.02]) # position et dimensions de la barre
-choix_z = Slider(barre_z, 'Amortissement (z)', 0, 10, valinit = 0)
-
-def update(val):
-    global T
-    xi = choix_z.val
-    module = 20*log10(absolute(H(W,w0,xi,K)[0]))
-    phase = angle(H(W,w0,xi,K)[0],'deg')
-    reponse.set_data(W,module)
-
-choix_z.on_changed(update)
+diag_pha,val=semilogx(W,phase,10,1) #Tracé en semilog de la phase
+# Paramétrage de la grille pour le module
+axes_pha = gca()
+axes_pha.set_ylabel('Phase (deg)')
+axes_pha.set_xlabel('Pulsation $(rad.s^{-1})$')
+axes_pha.grid(True,which='minor', alpha=0.2)
+axes_pha.grid(True,which='major', alpha=0.5)
 
 show()
--- S02 Systèmes Linéaires Continus Invariants/C03 Etude harmonique des SLCI/Trace_Bode.py
+++ S02 Systèmes Linéaires Continus Invariants/C03 Etude harmonique des SLCI/Trace_Bode.py
@@ -1,195 +1,157 @@
-# -*- coding: utf-8 -*-
-#-------------------------------------------------------------------------------
-# Animation de la réponse indicielle d'un système d'ordre 2 en fonction de 
-# l'amortissement (pour un système normé, K = 1, omega0 = 1)
-# et correspondance avec l'abaque du temps de réponse adimensionnel
-# Jean-Philippe COSTES - Lycée LESAGE - Vannes - Septembre 2014
-#-------------------------------------------------------------------------------
-import pylab as plt
-import numpy as np
-from matplotlib.widgets import Slider, RadioButtons
-from matplotlib.patches import Polygon
+# On importe les bibliothèques
+from matplotlib.pyplot import *
+from numpy import *
+from pylab import *
+from matplotlib.widgets import TextBox
 
-#-------------------------------------------------------------------------------
-# Réponse indicielle normée adimensionnelle d'un ordre 2: s(t,z)
-#-------------------------------------------------------------------------------
-def s(t, z):
-    """ fonction qui retourne la valeur de s(t) en fonction de s(t, z)
-        (fonction vectorisable car les opérateurs sont tous issus de numpy """
-    
-    b = z**2-1
-    
-    if z > 1:
-        a = np.sqrt(b)
-        return 1+np.exp(-t*(z+a))/2/(z*a+b)-np.exp(-t*(z-a))/2/(z*a-b)
-    
-    elif z < 1:
-        a = np.sqrt(-b)
-        return 1-np.exp(-z*t)/a*np.sin(t*a+np.arcsin(a))
+K=1
+w0=100
+xi=1
+xi_init=[0.001, 10]
+w0_init=[1, 1000]
+K_init=[1, 10]
 
-    else:                           # cas où z == 1
-        return 1-(1+t)*np.exp(-t)
-    
-#-------------------------------------------------------------------------------
-# définition d'un intervalle de temps réduit T = t * wo
-#-------------------------------------------------------------------------------
-tmin  = 0    # borne minimale (incluse)
-tmax  = 15.  # borne maximale (incluse)
-delta = 0.01 # choix d'un incrément pour les valeurs discrètes de T
+def H(w,w0,xi,K):
+    return K/(1+2*xi*1j*w/w0-w**2/w0**2)
 
-T = np.arange(tmin, tmax+delta, delta)
+def plage_w(w0,xi):
+    if xi>1:
+        w1=w0*(xi+(xi**2-1)**(1/2))
+        w2=w0*(xi-(xi**2-1)**(1/2))
+        return log10(w2)-3,log10(w1)+3
+    else:
+        return log10(w0)-3,log10(w0)+3
 
-#-------------------------------------------------------------------------------
-# Construction d'une bande rectangulaire pour faire apparaitre les +/- 5%
-#-------------------------------------------------------------------------------
-eps = 0.05
-rectangle = [(0,1-eps), (0,1+eps), (tmax,1+eps), (tmax,1-eps)]
-bande5    = Polygon(rectangle, facecolor='0.8', edgecolor='0.5')
-
-#-------------------------------------------------------------------------------
-# définition d'une fonction qui permet de savoir si une valeur (v)
-# est dans l'intervalle [1-epsilon, 1+epsilon]
-# en pratique epsilon = 0.05 pour l'abaque du temps de réponse à +/-5%
-#-------------------------------------------------------------------------------
-def est_dans_bande(v, eps = 0.05):
-    """ fonction qui renvoie True(~1) si v est dans l'intervalle [1 +/-eps] """
-    return abs(v-1) <= eps
-
-#-------------------------------------------------------------------------------
-# fonction pour la recherche du temps de réponse à 5%
-#-------------------------------------------------------------------------------
-def recherche_tr5(z, tm = 20, delta = 0.01):
-    tr5 = tm
-    while est_dans_bande(s(tr5, z), eps):
-        tr5 -= delta
-
-    return tr5
-
-#-------------------------------------------------------------------------------
-# Tracé de l'abaque en échelle linéaire
-# stratégie: on balaye les z par valeur croissante
-# pour chaque z, on teste la sortie de bande en partant d'une valeur de Tr
-# suffisamment grande (Trmax pour commencer) mais pour la suite,
-# on ne repart pas de Trmax (gain de temps),
-# mais de Tr5[i-1] auquel on rajoute 1% (arbitrairement),
-# car Tr5% n'est pas strictement décroissant sur z = [0, 0.69],
-# et pour z = [0.69 , +infini], les variations de Tr5% sont faibles
-#-------------------------------------------------------------------------------
-# définition d'une discrétisation en z et en Tr(temps de réponse)
-#-------------------------------------------------------------------------------
-zmin  = 0.1    # borne minimale pour l'amortissement (incluse)
-zmax  = 2      # borne maximale (incluse)
-pas_z = 0.001  # choix d'un incrément pour les valeurs de z
-
-Z     = np.arange( zmin , zmax+pas_z , pas_z)
-Tr5   = []    # initialisation de la liste des valeurs de tr5
-
-#-------------------------------------------------------------------------------
-# recherche du temps de réponse à 5%
-#-------------------------------------------------------------------------------
-t = tmax
-
-for z in Z:
-    Tr5.append(recherche_tr5(z, tm = t, delta = delta))
-    t = Tr5[-1]*1.05 # on repart de Tr5 précédent auquel on ajoute 5%
+def plage_mod(module):
+    global axes_mod
+    delta=(max(module)-min(module))/10
+    axes_mod.set_ylim(((min(module)-delta)//10)*10, ((max(module)+delta)//10+1)*10)
+    major_ticks_y = np.arange(((min(module)-delta)//10-1)*10, ((max(module)+delta)//10+1)*10+1, (((max(module)-min(module))/5)//5)*5)
+    minor_ticks_y = np.arange(((min(module)-delta)//10-1)*10, ((max(module)+delta)//10+1)*10+1, (((max(module)-min(module))/20)//5)*5)
+    axes_mod.set_yticks(major_ticks_y)
+    axes_mod.set_yticks(minor_ticks_y, minor=True)
+    axes_mod.grid(True,which='minor', alpha=0.2)
+    axes_mod.grid(True,which='major', alpha=0.5)
 
 
-#-------------------------------------------------------------------------------
-# Tracé de la réponse paramétrable (en z) et de l'abaque 
-#-------------------------------------------------------------------------------
-T = np.arange(tmin, tmax+delta, delta)
+fig=figure('Diagrammes Bode')
+bornes_w=plage_w(w0,xi)
+puissances_w=arange(bornes_w[0],bornes_w[1],0.01)
+W=10**puissances_w
+# La phase en degré
+phase = angle(H(W,w0,xi,K),'deg')
+# Le module en dB
+module = 20*log10(absolute(H(W,w0,xi,K)))
+#Tracer du diagramme de Bode
+subplot(311) # Permet d’afficher plusieurs graphes (nombre de graphe (2), colonne (1), ligne (1))
+diag_mod,val=semilogx(W,module,10,1) # Tracé en semilog du module
+axes_mod = gca()
+axes_mod.set_ylabel('Gain (dB)')
+axes_mod.set_xscale('log')
+plage_mod(module)
+subplot(312)
+diag_pha,val=semilogx(W,phase,10,1) #Tracé en semilog du module
+axes_pha = gca()
+axes_pha.set_ylim((min(phase)//30)*30-30, (max(phase)//30*30)+30)
+axes_pha.set_ylabel('Phase (deg)')
+axes_pha.set_xlabel('Pulsation $(rad.s^{-1})$')
+major_ticks_y = np.arange((min(phase)//30*30)-30, ((max(phase)//30+1)*30)+30, 30)
+minor_ticks_y = np.arange((min(phase)//30*30)-30, ((max(phase)//30+1)*30)+30, 15)
+axes_pha.set_yticks(major_ticks_y)
+axes_pha.set_yticks(minor_ticks_y, minor=True)
+axes_pha.grid(True,which='minor', alpha=0.2)
+axes_pha.grid(True,which='major', alpha=0.5)
+barre_xi = plt.axes([0.2, 0.11, 0.3, 0.02]) # position et dimensions de la barre
+choix_xi = Slider(barre_xi, 'Amortissement (z)', xi_init[0], xi_init[1], valinit = xi)
+axbox_xi = fig.add_axes([0.8, 0.11, 0.1, 0.05])
+text_xi = TextBox(axbox_xi, "Evaluate", textalignment="center")
+barre_w0 = plt.axes([0.2, 0.2, 0.3, 0.02]) # position et dimensions de la barre
+choix_w0 = Slider(barre_w0, 'Pulsation propre (w0)', w0_init[0], w0_init[1], valinit = w0)
+axbox_w0 = fig.add_axes([0.8, 0.2, 0.1, 0.05])
+text_w0 = TextBox(axbox_w0, "Evaluate", textalignment="center")
+barre_K = plt.axes([0.2, 0.02, 0.3, 0.02]) # position et dimensions de la barre
+choix_K = Slider(barre_K, 'Gain (K)', K_init[0], K_init[1], valinit = K)
+axbox_K = fig.add_axes([0.8, 0.02, 0.1, 0.05])
+text_K = TextBox(axbox_K, "Evaluate", textalignment="center")
 
-fig, (ax1, ax2) = plt.subplots(ncols = 2, figsize = (16, 8))
-plt.subplots_adjust(left = 0.10, bottom = 0.2, wspace = 0.15)
-
-fs = 14 # fontsize pour les textes (titres, labels, etc)
-
-# dans le 1er dessin (gauche)
-plt.subplot(121)
-plt.xticks(np.linspace(0, int(tmax), int(tmax)+1))
-plt.yticks(np.arange(0, 2.25, 0.25))
-plt.grid(True, color = '0.5', linestyle = '-')
-plt.title(r'$Réponse\ indicielle\ (système\ du\ 2^{ième}\ ordre\ normé:\ \omega_0=1)$', fontsize = fs)
-plt.ylabel('$Réponse$', fontsize = fs)
-plt.text(tmax-1, -0.15, '$t(s)$', fontsize = fs)
-
-
-z0 = 1
-plt.axis([0,tmax,0,2])
-
-tr5_0 = recherche_tr5(z0)   # temps de réponse pour z = z0
-s0    = s(tr5_0,z0)         # valeur de s(t, z) pour t = tr5_0 et z = z0
-
-plt.plot(T, s(T,1), '--k', lw = 1.)       # réponse critique (z = 1)
-plt.plot([0,tmax], [1,1], '-k', lw = 1.)  # asymptote y = 1
-plt.plot(tr5_0, s0, 'ow')                # marker pour tr5 pour z = 1
-plt.plot([tr5_0, tr5_0], [0, s0], '--k')   # ligne de rappel de tr5% pour z = 1
-
-plt.fill([tmin, tmax, tmax, tmin, tmin], [1-eps, 1-eps, 1+eps, 1+eps, 1-eps],\
-         '-g', alpha = 0.3)
-
-Xline5 = [tr5_0, tr5_0]
-Yline5 = [0, s(tr5_0, z0)]
-Xtext5, Ytext5 = tr5_0, 0
-
-reponse, = plt.plot(T, s(T, z0), '-b', linewidth = 1.5)
-mark_t5, = plt.plot(tr5_0, s0, 'or')
-line_t5, = plt.plot(Xline5, Yline5, '-r')
-
-
-# dans le second dessin (droite)
-plt.subplot(122)
-plt.axis([0, zmax, 0, tmax])
-plt.xticks(np.arange(0, zmax+0.2, 0.2))
-plt.yticks(np.arange(0, tmax+1, 1))
-plt.grid(True, color = '0.5', linestyle = '-')
-plt.title(r'$Abaque\ du\ temps\ de\ réponse\ à\ 5\% \ (normé).$', fontsize = fs)
-plt.ylabel('$T_{R5\%}\ \cdot\ \omega_0$', fontsize = fs)
-plt.text(zmax*.75, -1.2, '$Amortissement\ z$', fontsize = fs)
-
-plt.plot(Z, Tr5, '-', color = 'grey')   # courbe continue (trame)
-
-# puis tracé de la courbe de l'abaque comme étant une courbe discontinue !
-# on commence par identifier les indices qui correspondent à une discontinuité
-# on en crée une liste I (qui comporte comme premier indice 0),
-# la recherche consiste à chercher les indices i tels que:
-# Tr5[i] et Tr5[i+1] sont distants de plus de 1
-I = [0]
-for i in range(len(Tr5)-1):
-    if Tr5[i] - Tr5[i+1] > 1: I.append(i)
-I.append(len(Tr5)-1)
-
-for k in range(len(I)-1):
-    plt.plot(Z[I[k]+1: I[k+1]+1], Tr5[I[k]+1: I[k+1]+1], '-k', lw = 2.) # abaque réel (discontinu)
-    
-#plt.plot(Z, Tr5, '.b', markersize = 1.) # abaque réel (discontinu)
-plt.plot(z0,tr5_0,'ow')                 # marqueur pour z = 1
-plt.plot([z0, z0],[0, tr5_0], '--k')    # ligne de rappel de tr5% pour z = 1
-
-mark_zt, = plt.plot(z0, tr5_0, 'or')
-
-zmax = 2 # redéfinition de zmax
-
-barre_z = plt.axes([0.2, 0.1, 0.4, 0.02]) # position et dimensions de la barre
-choix_z = Slider(barre_z, 'Amortissement (z)', 0, zmax, valinit = z0)
-barre_ordre = plt.axes([0.2, 0.67, 0.1, 0.1]) # position et dimensions de la barre
-choix_ordre = RadioButtons(barre_ordre, ['1er ordre', '2nd ordre'], active=1, activecolor='blue')
-choix_ordre.on_clicked(hzfunc)
 
 def update(val):
     global T
-    z = choix_z.val
-        
-    reponse.set_data(T, s(T,z))
-    tr5 = recherche_tr5(z)
-    mark_t5.set_data(tr5, s(tr5,z))
+    K = choix_K.val
+    w0 = choix_w0.val
+    xi = choix_xi.val
+    bornes_w=plage_w(w0,xi)
+    puissances_w=arange(bornes_w[0],bornes_w[1],0.01)
+    W=10**puissances_w
+    axes_mod.set_xlim(w0/1000,1000*w0)
+    axes_pha.set_xlim(w0/1000,1000*w0)
+    module = 20*log10(absolute(H(W,w0,xi,K)))
+    phase = angle(H(W,w0,xi,K),'deg')
+    diag_mod.set_data(W,module)
+    diag_pha.set_data(W,phase)
+    plage_mod(module)
 
-    mark_zt.set_data(z, tr5)
 
-    Xline5 = [tr5, tr5]
-    Yline5 = [0, s(tr5, z)]
-    line_t5.set_data(Xline5, Yline5)
+def update_K(expression):
+    if expression:
+        K = float(eval(expression))
+        choix_K.set_val(K)
+        if choix_K.valmax<K:
+            choix_K.ax.set_xlim(K_init[0],K)
+        elif choix_K.valmin>K:
+            choix_K.ax.set_xlim(K,K_init[1])
+        bornes_w=plage_w(w0,xi)
+        puissances_w=arange(bornes_w[0],bornes_w[1],0.01)
+        W=10**puissances_w
+        axes_mod.set_xlim(10**bornes_w[0],10**bornes_w[1])
+        axes_pha.set_xlim(10**bornes_w[0],10**bornes_w[1])
+        module = 20*log10(absolute(H(W,w0,xi,K)))
+        phase = angle(H(W,w0,xi,K),'deg')
+        diag_mod.set_data(W,module)
+        diag_pha.set_data(W,phase)
 
-choix_z.on_changed(update)
+def update_xi(expression):
+    if expression:
+        xi = float(eval(expression))
+        choix_xi.set_val(xi)
+        if choix_xi.valmax<xi:
+            choix_xi.ax.set_xlim(xi_init[0],xi)
+        elif choix_xi.valmin>xi:
+            choix_xi.ax.set_xlim(xi,xi_init[1])
+        bornes_w=plage_w(w0,xi)
+        puissances_w=arange(bornes_w[0],bornes_w[1],0.01)
+        W=10**puissances_w
+        axes_mod.set_xlim(10**bornes_w[0],10**bornes_w[1])
+        axes_pha.set_xlim(10**bornes_w[0],10**bornes_w[1])
+        module = 20*log10(absolute(H(W,w0,xi,K)))
+        phase = angle(H(W,w0,xi,K),'deg')
+        diag_mod.set_data(W,module)
+        diag_pha.set_data(W,phase)
 
-plt.show()
+def update_w0(expression):
+    if expression:
+        w0 = float(eval(expression))
+        choix_w0.set_val(w0)
+        if choix_w0.valmax<w0:
+            choix_w0.ax.set_xlim(w0_init[0],w0)
+        elif choix_w0.valmin>w0:
+            choix_w0.ax.set_xlim(w0,w0_init[1])
+        bornes_w=plage_w(w0,xi)
+        puissances_w=arange(bornes_w[0],bornes_w[1],0.01)
+        W=10**puissances_w
+        axes_mod.set_xlim(10**bornes_w[0],10**bornes_w[1])
+        axes_pha.set_xlim(10**bornes_w[0],10**bornes_w[1])
+        module = 20*log10(absolute(H(W,w0,xi,K)))
+        phase = angle(H(W,w0,xi,K),'deg')
+        diag_mod.set_data(W,module)
+        diag_pha.set_data(W,phase)
+
+    
+choix_K.on_changed(update)
+choix_w0.on_changed(update)
+choix_xi.on_changed(update)
+text_K.on_submit(update_K)
+text_w0.on_submit(update_w0)
+text_xi.on_submit(update_xi)
+
+show()
