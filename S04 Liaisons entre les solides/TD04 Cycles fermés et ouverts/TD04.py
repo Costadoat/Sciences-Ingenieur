b02d91d925d3bc6bfdf6e035bd805896899f8615
costadoat <costadoat@crans.org>
2023-12-12 22:59:52 +0100

    S04TD04

---
+++ S04 Liaisons entre les solides/TD04 Cycles fermés et ouverts/TD04.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a=45
+b=65
+
+theta=np.linspace(0,np.pi/2,100)
+u= #à compléter
+alpha= #à compléter
+beta= #à compléter
+
+x= #à compléter
+y= #à compléter
+
+plt.plot(x,y)
+plt.show()
