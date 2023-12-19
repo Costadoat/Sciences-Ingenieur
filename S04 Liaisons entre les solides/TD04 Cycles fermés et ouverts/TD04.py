8f2627c38be65d104712007b6ad504f445ad12a0
costadoat <costadoat@crans.org>
2023-12-19 19:16:11 +0100

    TD04

--- S04 Liaisons entre les solides/TD04 Cycles fermés et ouverts/TD04.py
+++ S04 Liaisons entre les solides/TD04 Cycles fermés et ouverts/TD04.py
@@ -5,12 +5,12 @@ a=45
 b=65
 
 theta=np.linspace(0,np.pi/2,100)
-u= #à compléter
-alpha= #à compléter
-beta= #à compléter
+u=np.sqrt(2)/2*(a+b)/np.cos(theta-np.pi/4)
+alpha=theta-np.arccos((u**2+a**2-b**2)/(2*u*a))
+beta=theta-alpha+np.arccos((u**2-a**2+b**2)/(2*u*b))
 
-x= #à compléter
-y= #à compléter
+x=a*np.cos(alpha)+b*np.cos(alpha+beta)
+y=a*np.sin(alpha)+b*np.sin(alpha+beta)
 
 plt.plot(x,y)
 plt.show()
